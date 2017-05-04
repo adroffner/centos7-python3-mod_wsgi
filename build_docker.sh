452a2a1b9f504443748f8dd5a1b2802084ba2eee
ITSERVICES\rh900r <rh900r@att.com>
2017-05-04 10:08:23 -0400

    [ODIN 5-4-17] changed all base image names to point to ATT Registry

--- build_docker.sh
+++ build_docker.sh
@@ -5,11 +5,14 @@
 # Build docker image
 # =============================================================================
 
-REPOSITORY="att-gcs"
+REGISTRY="zlp11313.vci.att.com:5100"
+NAMESPACE="com.att.dev.argos"
 IMAGE_NAME="centos7-python3-mod_wsgi"
 TAG="3.5.2"
 
-IMAGE_TAG="${REPOSITORY}/${IMAGE_NAME}:${TAG}"
+IMAGE_TAG="${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${TAG}"
+
+docker login -u m12292@argos.dev.att.com -p D4t4b4s3 -e m12292@att.com ${REGISTRY}
 
 docker build -t $IMAGE_TAG ./ \
     --build-arg http_proxy=$http_proxy \

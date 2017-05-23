61f713f07fa3aa39fec7966d10a38fb20066c704
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2017-05-23 17:08:20 -0400

    [DEV-000] Update the name of the FROM base image.

--- build_docker.sh
+++ build_docker.sh
@@ -8,7 +8,8 @@
 REGISTRY="zlp11313.vci.att.com:5100"
 NAMESPACE="com.att.dev.argos"
 IMAGE_NAME="centos7-python3-mod_wsgi"
-TAG="3.5.2"
+# TAG="3.5.2"
+TAG="latest"
 
 IMAGE_TAG="${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${TAG}"
 

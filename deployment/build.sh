bf6187381be0c57c86c560f3a0a981eb6e2af590
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2017-11-24 16:56:44 -0500

    [INC-2046] Created --latest (or -L) flag to build this as the "latest production release".

--- deployment/build.sh
+++ deployment/build.sh
@@ -8,11 +8,12 @@ source ./deployment/configs.env
 
 SUDO="sudo" # default
 PUSH_FLAG="true" # default
+LATEST_FLAG="" # default
 
 # =============================================================================
 
 # Execute getopt
-ARGS=`getopt -o "SP" -l "no-sudo,no-push" -n "getopt.sh" -- "$@"`
+ARGS=`getopt -o "SPL" -l "no-sudo,no-push,latest" -n "getopt.sh" -- "$@"`
 
 # Bad arguments
 if [ $? -ne 0 ]; then
@@ -33,12 +34,22 @@ while true; do
             PUSH_FLAG=""
             shift 1
         ;;
+        -L|--latest)
+            LATEST_FLAG="true"
+            shift 1
+        ;;
         --)
             shift 1
             break
         ;;
     esac
 done
+
+# Cannot set the --latest tag unless the image is pushed.
+if [ -z "$PUSH_FLAG" ] && [ "$LATEST_FLAG" ]; then
+    echo "WARNING: $0 --no-push is set...will not create --latest tag unless the image is pushed."
+    LATEST_FLAG=""
+fi
 
 # =============================================================================
 
@@ -54,6 +65,13 @@ $SUDO docker build -t $IMAGE_TAG \
 
 if [ "$PUSH_FLAG" ]; then
     $SUDO docker push $IMAGE_TAG
+fi
+
+# Create and push --latest.
+if [ "$PUSH_FLAG" ] && [ "$LATEST_FLAG" ]; then
+    LATEST_TAG="${IMAGE_TAG%:*}:latest"
+    $SUDO docker tag  $IMAGE_TAG  $LATEST_TAG
+    $SUDO docker push $LATEST_TAG
 fi
 
 $SUDO docker logout ${REGISTRY}
